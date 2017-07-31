fd3a3177cd39ea71a55d256455213216cab6086a
Unknown <devonnuri@gmail.com>
2017-07-31 20:38:25 +0900

    폼에 아무짓도 안했는데;; 죄송

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ session_start();
                     echo "있어";
                 }
             ?>
-            <form>
+            <form action="/request.php" method="post">
               <div class="form-group">
                 <label for="exampleInputEmail1">사용자 이름</label>
                 <input type="text" class="form-control" id="exampleInputEmail1" placeholder="사용자 이름을 입력해주세요.">
@@ -32,6 +32,7 @@ session_start();
               <button type="submit" class="btn btn-default">로그인</button>
             </form>
         </div>
+        <script src="http://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
 </html>
