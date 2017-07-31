ea47e1f948649f648bcc64f4a2fe6835de83b969
Unknown <devonnuri@gmail.com>
2017-07-31 19:39:20 +0900

    ㅁㄴㅇㄹ

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <title>로컬호스트</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>로컬호스트의 SQL Injection 시연 서버</h1>
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">사용자 이름</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="사용자 이름을 입력해주세요.">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">비밀번호</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              </div>
+              <button type="submit" class="btn btn-default">로그인</button>
+            </form>
+        </div>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
